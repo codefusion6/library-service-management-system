41bc66e3c21d133c04a0be500dcd3fd69dff1a86
rafahuqe <arinahuqe23@gmail.com>
2024-01-16 22:32:29 +0600

    add extra featured for form

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const login = () => {
   return (
     <div>
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-col border justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           {/* <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -76,7 +76,7 @@ const login = () => {
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?
             <Link href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Please Register
+              Please Sign Up
             </Link>
           </p>
         </div>
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const register = () => {
   return (
     <div>
-      <div className="bg-grey-lighter min-h-screen flex flex-col">
+      <div className="bg-grey-lighter  mt-10 mb-10 min-h-screen flex flex-col">
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">Sign up</h1>
@@ -38,7 +38,7 @@ const register = () => {
               type="submit"
               className="w-full text-center py-3 rounded bg-blue-600 text-white hover:bg-green-dark focus:outline-none my-1"
             >
-              Create Account
+              Sign Up
             </button>
 
             <div className="text-center text-sm text-grey-dark mt-4">
