e9d0b5616656ec4f79381d23f1cce92e7d887670
rafahuqe <arinahuqe23@gmail.com>
2024-01-16 23:40:40 +0600

    install lottie animation

---
+++ src/components/layout/animation/LottieAnimation.jsx
@@ -0,0 +1,16 @@
+// components/LottieAnimation.js
+import React from 'react';
+import Lottie from 'lottie-react';
+ // Update the path
+
+const LottieAnimation = () => {
+  return (
+    <Lottie
+      animationData={animationData}
+      loop
+      autoplay
+    />
+  );
+};
+
+export default LottieAnimation;
