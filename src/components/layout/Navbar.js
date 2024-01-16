2ac88ff2b3f1a8aab525283cd5e9f45ef58cc8ff
probeshnath <probeshn@gmail.com>
2024-01-16 17:56:08 +0600

    navbar update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,8 +4,8 @@ import { FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className='max-w-7xl bg-red-300 mx-auto flex justify-between items-center px-3 lg:px-0 py-3'>
+    <div className='bg-red-300'>
+      <div className='max-w-7xl  mx-auto flex justify-between items-center px-3 py-3'>
         <h2>Book House</h2>
         <div>
           <FaBars className='md:hidden' />
@@ -14,8 +14,11 @@ const Navbar = () => {
             <li><Link href="/">Home</Link></li>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/">Home</Link></li>
-            <li><Link href="/">Sign In</Link></li>
           </ul>
+        </div>
+        <div className='flex gap-4'>
+          <button className='bg-green-700 px-3 rounded-3xl text-md text-white'><Link href="/">Sign In</Link></button>
+          <img className='w-10 h-10 rounded-full' src="https://reputationprotectiononline.com/wp-content/uploads/2022/04/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" />
         </div>
       </div>
     </div>
