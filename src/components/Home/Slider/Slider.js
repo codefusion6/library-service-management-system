0fd73f1d21afa0b0f46c4487110b28ebd23aec78
sumonpaul55 <sumpaul100@gmail.com>
2024-01-17 20:23:03 +0600

    swiper installed

--- src/components/Home/Slider/Slider.js
+++ src/components/Home/Slider/Slider.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Swiper from "./Swiper";
+import Swiper from "swiper";
 
 const Slider = () => {
   return (
     <section className="container mx-auto my-16 ">
       <p className="text-3xl font-bold text-center mb-10">Our Top Author &apos;s</p>
-    
 
       <Swiper></Swiper>
-      
+
     </section>
   );
 };
