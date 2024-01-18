dfa49fd0ed58663e9652b8cc72a9a1e3b127b729
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-01-19 00:20:01 +0600

    design the footer and contact section

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -6,22 +6,22 @@ import { MdOutlineMail } from "react-icons/md";
 
 const Contact = () => {
   return (
-    <div className="bg-[#181818] text-white">
+    <div className="bg-[#111111] text-white">
       <div className="container p-10">
         <h1 className="text-3xl font-bold text-center mb-8">Contact</h1>
 
         <div className="grid grid-cols-4 items-center justify-center gap-5">
-          <a href="" className="border border-[#ddb425] mx-auto rounded-full h-40  p-2">
-            <MdOutlineMail className="w-48 h-40" />
+          <a href="" className="border border-[#ddb425] mx-auto justify-center rounded-full h-32  p-5">
+            <MdOutlineMail className="w-20 h-20" />
           </a>
-          <a href="" className="border border-[#ddb425] mx-auto rounded-full h-40  p-2">
-            <FaFacebookF className=" w-48 h-36 " />
+          <a href="" className="border justify-center border-[#ddb425] mx-auto rounded-full h-32  p-5">
+            <FaFacebookF className=" w-20 h-20 " />
           </a>
-          <a href="" className="border border-[#ddb425] mx-auto rounded-full h-40  p-2">
-            <FiGithub className=" w-48 h-36  " />
+          <a href="" className="border justify-center border-[#ddb425] mx-auto rounded-full h-32  p-5">
+            <FiGithub className=" w-20 h-20  " />
           </a>
-          <a href="" className="border border-[#ddb425] mx-auto rounded-full h-40  p-2">
-            <FaPhoneAlt className="w-48 h-32 " />
+          <a href="" className="border justify-center border-[#ddb425] mx-auto rounded-full h-32  p-5">
+            <FaPhoneAlt className="w-20 h-20" />
           </a>
         </div>
       </div>
