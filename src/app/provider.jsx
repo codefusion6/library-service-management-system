229646fbcbb7bc12a9a6da844a77dd2126883a5a
sumonpaul55 <sumpaul100@gmail.com>
2024-01-21 18:38:23 +0600

    navbar layout and nexttheme provider setup

---
+++ src/app/provider.jsx
@@ -0,0 +1,19 @@
+// app/providers.tsx
+"use client";
+import React from 'react'
+
+
+import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+function NextThemeProvider({ children }) {
+    return (
+        <NextUIProvider>
+            <NextThemesProvider>
+                {children}
+            </NextThemesProvider>
+        </NextUIProvider>
+    )
+}
+
+export default NextThemeProvider
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 const { nextui } = require("@nextui-org/react");
-
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js'
+    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js',
+
   ],
   theme: {
 
@@ -22,6 +22,6 @@ module.exports = {
       },
     },
   },
-  
+  darkMode: "class",
   plugins: [nextui()],
 }
