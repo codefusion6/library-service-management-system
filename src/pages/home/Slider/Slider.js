12e9dd8f4771dc5e7075bebda107f4650049d596
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-01-21 19:51:07 +0600

    author slider done

--- src/pages/home/Slider/Slider.js
+++ src/pages/home/Slider/Slider.js
@@ -10,7 +10,7 @@ const Slider = () => {
       <p className="text-center text-lg font-medium">
       Dive into the literary world with our curated selection of renowned authors, each contributing their unique perspective to the tapestry of storytelling.Explore the profound works of celebrated authors who have left an indelible mark on literature, captivating readers with their eloquent prose and thought-provoking narratives.Join us in celebrating the literary legacy of these iconic authors, whose contributions have shaped the literary landscape and enriched the cultural heritage of storytelling.
       </p>
-      <div>
+      <div className="">
         <Swiper></Swiper>
       </div>
     </section>
