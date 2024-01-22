8046ca858226c959d67a6f21d5dac49376dd3525
sumonpaul55 <sumpaul100@gmail.com>
2024-01-22 18:34:42 +0600

    banner animation textanimation done

--- src/pages/home/Banner.js
+++ src/pages/home/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
     <section className="min-h-screen flex items-center justify-center py-20 lg:py-0 px-1" >
       <div className="container mx-auto ">
         <div className="justify-content-center align-items-center grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-3">
-          <div className="space-y-6">
+          <div className="flex flex-col justify-around gap-2">
             <h2 className="text-3xl md:min-h-20 sm:text-7xl lg:text-5xl xl:text-7xl py-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-green-600">
               <TypeAnimation
                 sequence={["", 1000, "Library Service Management", 1000]}
@@ -18,29 +18,31 @@ const Banner = () => {
                 repeat={Infinity}
               />
             </h2>
-            <p className="md:text-xl font-base md:font-normal text-left">
-              &quot;Explore a world of knowledge with our Library Service
-              Management! Borrow, bookmark, and read your favorite books
-              effortlessly. Unlock the joy of reading with our user-friendly
-              platform. Your gateway to an enriched reading experience
-              awaits!&quot;
-            </p>
-            <div className="flex gap-5 font-medium">
-              <Button
-                color="primary"
-                variant="bordered"
-                className="font-bold "
-                size="lg"
-              >
-                Explore
-              </Button>
-              <Button
-                color="primary"
-                className="bg-gradient-to-tr from-yellow-200 to-[#1ba752] text-white shadow-lg"
-                size="lg"
-              >
-                Buy Now
-              </Button>
+            <div className="flex flex-col gap-12">
+              <p className="md:text-xl font-base md:font-normal text-left">
+                &quot;Explore a world of knowledge with our Library Service
+                Management! Borrow, bookmark, and read your favorite books
+                effortlessly. Unlock the joy of reading with our user-friendly
+                platform. Your gateway to an enriched reading experience
+                awaits!&quot;
+              </p>
+              <div className="flex gap-5 font-medium">
+                <Button
+                  color="primary"
+                  variant="bordered"
+                  className="font-bold "
+                  size="lg"
+                >
+                  Explore
+                </Button>
+                <Button
+                  color="primary"
+                  className="bg-gradient-to-tr from-yellow-200 to-[#1ba752] text-white shadow-lg"
+                  size="lg"
+                >
+                  Buy Now
+                </Button>
+              </div>
             </div>
           </div>
           <div>
