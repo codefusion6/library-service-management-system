817d24c26ed1614059a72fadf7731f934db8e1b9
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-01-22 20:10:17 +0600

    navbar fixed

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -4,16 +4,15 @@ import { MdDarkMode } from "react-icons/md";
 import { MdOutlineDarkMode } from "react-icons/md";
 
 export const ThemeSwitcher = () => {
-    const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
-    return (
-        <div className="">
-            {
-                theme === "dark" ? <MdDarkMode size={25} onClick={() => setTheme('purple-dark')} /> :
-                    <MdOutlineDarkMode size={25} onClick={() => setTheme('light')} />
-            }
-
-
-        </div>
-    )
+  return (
+    <div className="">
+      {theme === "dark" ? (
+        <MdDarkMode size={25} onClick={() => setTheme("purple-dark")} />
+      ) : (
+        <MdOutlineDarkMode size={25} onClick={() => setTheme("light")} />
+      )}
+    </div>
+  );
 };
--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -16,8 +16,11 @@ import {
 import Image from "next/image";
 import logo from "../../public/images/bookflow.png";
 import { GiHamburgerMenu } from "react-icons/gi";
-
 import { CgProfile } from "react-icons/cg";
+import { UserAuth } from "@/app/context/AuthContext";
+
+
+
 
 const Nav = () => {
   const [show, setShow] = useState(false);
