6ec63cb246c8d065549893b0f2cf43ff0ebe319b
hossainAbirr <hossainabirbusiness@gmail.com>
2024-01-22 20:33:25 +0600

    reg form edited

--- src/app/context/AuthContext.jsx
+++ src/app/context/AuthContext.jsx
@@ -5,19 +5,25 @@ import {
   signOut,
   onAuthStateChanged,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../firbase/firebase";
 
-const AuthContext = createContext();
+ const AuthContext = createContext(null);
 
-export const AuthContextProvider = ({ children }) => {
+ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   const googleSignIn = async () => {
     const provider = new GoogleAuthProvider();
     await signInWithPopup(auth, provider);
   };
+
+  const createUser = (email, password, fullname) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password, fullname);
+  }
 
   const logOut = () => {
     signOut(auth);
@@ -27,8 +33,8 @@ export const AuthContextProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       setLoading(false);
-          // Log photoURL if available
-    if (currentUser && currentUser.photoURL) {
+      // Log photoURL if available
+      if (currentUser && currentUser.photoURL) {
         console.log('User photoURL:', currentUser.photoURL);
       }
     });
@@ -36,7 +42,7 @@ export const AuthContextProvider = ({ children }) => {
   }, [user]);
 
   return (
-    <AuthContext.Provider value={{ user, googleSignIn, logOut, loading}}>
+    <AuthContext.Provider value={{ user, googleSignIn, logOut, loading, createUser }}>
       {children}
     </AuthContext.Provider>
   );
