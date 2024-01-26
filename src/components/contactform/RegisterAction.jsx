2557a006d937830a5cd7ca896b52913de5250b70
hossainAbirr <hossainabirbusiness@gmail.com>
2024-01-26 18:23:20 +0600

    add book form added with revalidate

--- src/components/contactform/RegisterAction.jsx
+++ src/components/contactform/RegisterAction.jsx
@@ -108,22 +108,22 @@ const RegisterAction = () => {
           </div>
         </div>
         <div class="mt-5">
-         
-            <button
-              className="flex mb-2 items-center justify-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg px-4 py-2"
-              onClick={handleGoogleSignIn}
-            >
-              <FaGoogle className="text-xl" />
-              <span className="ml-2">Sign in with Google</span>
-            </button>
 
-            <button
-              className="bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg px-4 py-2"
-              type="submit"
-            >
-              Sign In
-            </button>
-         
+          <button
+            className="flex mb-2 items-center justify-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg px-4 py-2"
+            onClick={handleGoogleSignIn}
+          >
+            <FaGoogle className="text-xl" />
+            <span className="ml-2">Sign in with Google</span>
+          </button>
+
+          <button
+            className="bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg px-4 py-2"
+            type="submit"
+          >
+            Sign In
+          </button>
+
         </div>
       </form>
 
