93b0ae189ae8df5808d42a406d73a9befecc3a16
hossainAbirr <hossainabirbusiness@gmail.com>
2024-01-26 21:15:08 +0600

    error handled

--- src/app/(admin)/dashboard/addbook/page.jsx
+++ src/app/(admin)/dashboard/addbook/page.jsx
@@ -2,15 +2,13 @@
 // Import necessary components and libraries
 import AddBookBanner from '@/pages/add-book/AddBookBanner';
 import AddBookForm from './AddBookForm';
-import { getAllBooks } from '@/libs/actions/addBook.action';
-import { connectDB } from '@/libs/database/MongoConnect';
 
 // Define AddBookFormPage component
 const AddBookFormPage = async () => {
   return (
     <section>
       <AddBookBanner />
-      {/* {/ Book Form /} */}
+      {/* Book Form  */}
       <AddBookForm></AddBookForm>
     </section>
   );
