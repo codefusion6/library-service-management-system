bc3403b41fbbc084c2d8853e74c37f27d583df8d
sumonpaul55 <sumpaul100@gmail.com>
2024-01-26 13:14:27 +0600

    author slider and dashboard side bar

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -6,10 +6,9 @@ function dashboardLayout({ children }) {
   return (
     <>
       <div className='flex gap-2'>
-        <Sidebar>
-          <SidebarItems ></SidebarItems>
-        </Sidebar>
-        <div className="flex-1">{children}</div>
+        <Sidebar></Sidebar>
+        <div className="flex-1">{children}
+        </div>
       </div>
     </>
   )
