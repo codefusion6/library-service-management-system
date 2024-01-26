2e30e13ca8c27a02098fdafb60a91a65ff635cbe
sumonpaul55 <sumpaul100@gmail.com>
2024-01-26 12:18:31 +0600

    side bar scroll bar done now need to fix the

--- src/app/(admin)/dashboard/page.jsx
+++ src/app/(admin)/dashboard/page.jsx
@@ -7,7 +7,7 @@ import Chart from "@/components/dashboardChart/Chart";
 
 function Dashboard({ children }) {
   return (
-    <main className="min-h-screen">
+    <main className="h-[100vh] relative overflow-y-scroll top-0 p-3 md:p-5 lg:p-8">
       <div className="">
         <div className="flex justify-end items-center gap-3 bg-gradient-to-r from-yellow-200 to-green-600 p-5">
           <input
@@ -47,7 +47,6 @@ function Dashboard({ children }) {
                 </p>
               </div>
             </CardHeader>
-
             <CardBody>
               <p>
                 This number is static now in next milestone we will show the
@@ -65,7 +64,6 @@ function Dashboard({ children }) {
                 </p>
               </div>
             </CardHeader>
-
             <CardBody>
               <p>
                 This number is static now in next milestone we will show the
@@ -77,6 +75,7 @@ function Dashboard({ children }) {
         <div className="w-96 mx-auto mt-5">
           <Chart></Chart>
         </div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab eligendi magnam natus. Reprehenderit asperiores porro suscipit repellat accusantium ipsum ea sit, temporibus expedita, earum, blanditiis quasi facilis sunt quas quisquam iusto dolorem amet qui. In quasi ducimus quam similique voluptate perspiciatis itaque recusandae alias quae nemo, suscipit eos molestias. Libero vero reprehenderit obcaecati nobis laborum totam quae exercitationem ad! Eligendi similique voluptatem harum, alias ipsum recusandae mollitia exercitationem autem itaque aut magnam molestias rem porro non nam sapiente quisquam inventore perspiciatis fuga ad ex esse distinctio. Fugiat beatae eum odio ad voluptates culpa dignissimos corrupti pariatur quia minus cumque, necessitatibus quidem! Dolorum reprehenderit dignissimos repellendus corrupti accusantium facilis quaerat vel illum omnis repellat. Fugit aliquam neque maiores, voluptate non exercitationem molestiae. Magnam inventore consectetur distinctio placeat. Ad quaerat maxime aliquid, culpa fuga et dolorem, optio vel perferendis at laboriosam non voluptatum deleniti inventore? Dicta aliquam quibusdam, assumenda architecto fuga numquam necessitatibus nulla quasi qui accusantium quos nobis, facere molestias ab nesciunt asperiores ea ratione tempora obcaecati debitis maxime, explicabo soluta. Sed deleniti saepe dicta in asperiores voluptatum quas natus omnis dignissimos dolorem recusandae consequatur suscipit voluptatem voluptates, quo est adipisci porro voluptatibus quia totam aut pariatur tempore accusantium? Voluptatem quisquam optio quidem nulla officiis error eos quos dolorem consequuntur ipsa. Voluptatibus dolor libero, temporibus excepturi perferendis asperiores accusantium nam quisquam. Optio molestiae autem suscipit cumque vero culpa consequatur est voluptas, rerum possimus nemo magnam quisquam at dolorum praesentium velit quas nulla voluptatum tempora doloribus unde laboriosam sunt! Nulla ut facilis sunt doloribus cum fugiat illum in vel nesciunt ex ea labore itaque sint, exercitationem ipsam debitis iste vitae soluta fuga ab repudiandae! Vitae at ipsa quia, repellat rerum, illum voluptas ullam harum corporis repellendus excepturi, officiis dolorum rem laborum maiores ut? Est quidem explicabo quibusdam illo hic temporibus, facere harum dolore nisi rem fuga nostrum itaque reiciendis ipsa exercitationem! Dolore sit culpa ut soluta quas, porro excepturi pariatur saepe nisi non iusto in sed ex velit delectus est debitis sapiente voluptas. Voluptas blanditiis aliquam ipsum, explicabo laborum dolores magnam quos veniam qui, dolorum praesentium quam consequuntur saepe esse quas! Assumenda sint dolorem, dolor consequuntur cum nulla accusamus ipsam pariatur tenetur natus eaque provident fuga fugiat iusto ad? Recusandae, cumque quod sapiente doloribus ad autem porro beatae perspiciatis impedit est repellat labore fugit earum ratione quam ducimus numquam exercitationem laborum iste reiciendis. Amet doloremque asperiores ratione esse eum quas. Ab, facilis?
       </div>
     </main>
   );
--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -1,21 +1,18 @@
-// import NextThemeProvider from "../provider/NextThemeProvider";
 import Sidebar from "./dashboard/Sidebar";
 import SidebarItems from "./dashboard/SidebarItems";
+import "./../globals.css"
+
 function dashboardLayout({ children }) {
   return (
     <>
-      <div className="flex gap-2 md:flex-row flex-col relative">
-        <div className="w-full min-h-screen md:w-3/12 border-r text-white fixed z-10 top-0 left-0 overflow-x-hidden overflow-scroll">
-          <Sidebar>
-            <SidebarItems></SidebarItems>
-          </Sidebar>
-        </div>
-        <div className="w-full md:w-9/12 bg-white p-4 md:p-10 ml-[358px]">
-          {children}
-        </div>
+      <div className='flex gap-2'>
+        <Sidebar>
+          <SidebarItems ></SidebarItems>
+        </Sidebar>
+        <div className="flex-1">{children}</div>
       </div>
     </>
-  );
+  )
 }
 
-export default dashboardLayout;
+export default dashboardLayout
