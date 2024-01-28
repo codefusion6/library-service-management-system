19c14158cf8181156b984ade54cac64bfc053684
sumonpaul55 <sumpaul100@gmail.com>
2024-01-28 18:27:45 +0600

    loading added

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -7,13 +7,12 @@ function dashboardLayout({ children }) {
   return (
     <>
       <div className='flex gap-2'>
-        <Sidebar>
-        </Sidebar>
-        <div className="flex-1 h-[100vh] relative overflow-y-scroll top-0 p-3 md:p-5 lg:p-8">{children}
+        <Sidebar></Sidebar>
+        <div className="flex-1 h-[100vh] relative overflow-y-scroll top-0 p-3 md:p-5 lg:p-8">
+          {children}
         </div>
       </div>
     </>
   )
 }
-
 export default dashboardLayout
--- src/app/loading.js
+++ src/app/loading.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Loading() {
     return (
-        <div className='flex items-center gap-1 w-full justify-center bg-black h-full bg-opacity-20 fixed top-0 left-0'>Loading... <div className='w-10 h-10 bg-primary animate-spin'></div></div>
+        <div className='flex items-center gap-1 w-full justify-center mt-20'>Loading... <div className='w-10 h-10 bg-primary animate-spin'></div>
+        </div>
     )
 }
-
 export default Loading
