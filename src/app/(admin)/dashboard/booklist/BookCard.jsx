6ab937d81aa51cdd170e41053e755d2b31f4171c
hossainAbirr <hossainabirbusiness@gmail.com>
2024-01-30 23:50:15 +0600

    build checked

--- src/app/(admin)/dashboard/booklist/BookCard.jsx
+++ src/app/(admin)/dashboard/booklist/BookCard.jsx
@@ -15,8 +15,7 @@ const BookCard = async ({id}) => {
 //   const res = await getBook("65b8dc882c1a2b234add17f0");
   console.log(id);
   const data = await getAllBooks();
-
-
+  console.log(data.books[0]._id, "from data");
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-10">
@@ -36,7 +35,7 @@ const BookCard = async ({id}) => {
             </CardBody>
             <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 group-hover:opacity-50 transition-opacity"></div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center space-x-4 opacity-0 group-hover:opacity-100 transition-opacity gap-3">
-              <Link href={`/dashboard/${book._id}`}>
+              <Link href={`/dashboard/booklist/${book._id}`}>
                 <FaEye className="text-3xl text-white" />
                 <span className="text-white">View</span>
               </Link>
