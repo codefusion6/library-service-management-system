38b964edfe3e1e2dda9366bcb2238d1f68c2249f
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-01-31 21:46:25 +0600

    allBooks page data fetch done

---
+++ src/app/(root)/allBooks/AllBooksSlider.jsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import "./styles.css";
+
+import { EffectCube, Pagination } from "swiper/modules";
+
+const AllBooksSlider = () => {
+  return (
+    <div>
+      <Swiper
+        effect={"cube"}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={true}
+        modules={[EffectCube, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default AllBooksSlider;
