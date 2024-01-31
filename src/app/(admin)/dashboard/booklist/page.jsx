fec24dd677eee2b993e9010d6d4181fe2d6ad154
hossainAbirr <hossainabirbusiness@gmail.com>
2024-01-31 09:05:37 +0600

    page optimized

--- src/app/(admin)/dashboard/booklist/page.jsx
+++ src/app/(admin)/dashboard/booklist/page.jsx
@@ -1,28 +1,15 @@
 
-import { addManyBook, getAllBooks, getBook } from "@/libs/actions/book.action";
-import { connectDB } from "@/libs/database/MongoConnect";
 import DashBookSlider from "@/components/dashBookSlider/DashBookSlider";
 import BookCard from "./bookCard";
 
-
-const BookListPage = async ({ params }) => {
-  console.log(params, "params");
-  
-  await connectDB();
-  const res = await getBook("65b8dc882c1a2b234add17f0")
-  console.log(res);
-  const data = await getAllBooks();
-  // console.log(data);
-
+const BookListPage = async() => {
   return (
     <section className="container mx-auto">
       <h2 className="text-3xl text-center my-5 font-medium text-green-700">
         Available Books List
-        
       </h2>
       <DashBookSlider></DashBookSlider>
-      <BookCard params={params.id}></BookCard>
-      
+      <BookCard></BookCard>
     </section>
   );
 };
