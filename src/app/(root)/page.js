28348f6c2e24820632a7f2aa121fc9ec032f2a69
sumonpaul55 <sumpaul100@gmail.com>
2024-02-01 02:00:14 +0600

    root page conflict

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -6,13 +6,9 @@ import FAQ from "@/pages/home/faq/FAQ";
 import Slider from "@/pages/home/Slider/Slider";
 import HomeContact from "@/shared/HomeContact";
 import Feature from "@/pages/home/Feature";
-<<<<<<< HEAD
-=======
-import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 import Chatbot from "@/components/chatbot/chatbot";
 
 
->>>>>>> dc9cffae9103359b38bb7444407727df4a4836d6
 
 const HomePage = () => {
   return (
