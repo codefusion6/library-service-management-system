26c63927ac3f4486e498e19e9a0e15f37ff6d0e5
sumonpaul55 <sumpaul100@gmail.com>
2024-02-03 12:59:25 +0600

    next config and middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
-export function middleware(request) {
+export async function middleware(request) {
   const cookieStore = cookies();
   const user = cookieStore.get('user')
   if (!user) {
