03c20b73e183a646f8808de1de6e93657f21e180
probeshnath <probeshn@gmail.com>
2024-02-06 20:33:45 +0600

    add route in dashboard

--- src/app/(admin)/dashboard/SidebarItems.js
+++ src/app/(admin)/dashboard/SidebarItems.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import React from "react";
 import { CiBoxList } from "react-icons/ci";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaUserPlus } from "react-icons/fa";
 import { MdFormatListBulletedAdd } from "react-icons/md";
+
 
 const SidebarItems = ({ expanded }) => {
   return (
@@ -33,6 +34,15 @@ const SidebarItems = ({ expanded }) => {
         <Link href="/dashboard/addbook" className="flex gap-2 font-bold">
           <MdFormatListBulletedAdd size={25} className="text-xl font-bold" />
           <sapn className={`${expanded ? "block" : "hidden"}`}>Add Book</sapn>
+        </Link>
+      </li>
+      <li
+        className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer
+      transition-colors group hover:bg-yellow-100"
+      >
+        <Link href="/dashboard/addwriter" className="flex gap-2 font-bold">
+          <FaUserPlus size={25} className="text-xl font-bold" />
+          <sapn className={`${expanded ? "block" : "hidden"}`}>Add Writer</sapn>
         </Link>
       </li>
       <li
