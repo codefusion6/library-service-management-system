899904e4ecd36cd7b453db66669db18db75e1309
probeshnath <probeshn@gmail.com>
2024-02-06 22:27:34 +0600

    add style in writer detail page

--- src/app/(root)/all-writers/[id]/page.jsx
+++ src/app/(root)/all-writers/[id]/page.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='pt-28'>
-            <div>
-                <div>
-                    <img src="https://i.ibb.co/686tyHs/probesh-deb-nath.jpg" alt="" />
+        <div className='pt-28 container mx-auto'>
+            <div className='grid grid-cols-1 md:grid-cols-3 py-5 md:gap-10  px-3 md:px-0'>
+                <div className='w-full mx-auto pb-10 md:pb-0'>
+                    <img src="https://i.ibb.co/686tyHs/probesh-deb-nath.jpg" className='w-full h-[300px] mx-auto' alt="" />
                 </div>
-                <div>
-                    <h2>Writer Name Here</h2>
-                    <hr />
-                    <p><strong>Address</strong> <span>Dhaka, Newmarket</span></p>
-                    <p><strong>Date of Birth</strong> <span>10 july, 1950</span></p>
-                    <p><strong>Date of Dead</strong> <span>09 Auguest, 2000</span></p>
-                    <p><strong>Address</strong> <span>Dhaka, Newmarket</span></p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio fuga dicta dolor nisi accusantium velit expedita tempore autem quibusdam dolorem pariatur neque a sunt dolore, excepturi consequuntur. Doloribus modi iusto veritatis impedit, molestiae libero laborum pariatur alias cumque ipsum porro id, voluptas blanditiis. Cum non, nemo, odit dicta fugiat voluptatum a harum commodi distinctio tenetur ut impedit quasi, eos laborum ad est quod provident culpa! Natus, dolorem voluptate. Sint, consectetur adipisci veritatis quia placeat dicta earum eligendi. Porro consequuntur unde iusto ab mollitia, iste dolor, omnis quod delectus laboriosam sed, dicta alias dolorem modi! Impedit iste nemo voluptatibus maiores. Totam dicta nam velit ea, iure impedit esse beatae rem voluptate voluptatum modi labore vel itaque pariatur aut. Eos, maiores? Expedita, vel natus. Qui, molestiae. Praesentium animi ipsam cum aut id minima alias repudiandae voluptate maiores, culpa exercitationem impedit eligendi sapiente voluptatum. Consequuntur explicabo ut ipsam eaque, aperiam dignissimos repellat mollitia, totam quidem odio atque eius excepturi quia asperiores nostrum est sint iure quam molestiae pariatur nihil blanditiis. Enim ipsam, nihil voluptas nulla ullam quaerat ipsa, architecto tenetur temporibus eos non molestias explicabo exercitationem tempora tempore animi dolorum culpa, esse provident officiis eaque dolor laborum rem praesentium? Fugiat accusantium officia impedit consectetur perferendis ab amet qui inventore provident minima placeat perspiciatis facilis ullam eveniet voluptatem est voluptas laudantium cupiditate aliquam tenetur, animi natus doloribus numquam molestias? Corporis, commodi numquam culpa animi aspernatur rerum autem asperiores, accusantium quas architecto nesciunt nostrum? Laborum similique consequuntur ab voluptatum eligendi, enim, fugiat aliquam odio voluptate laudantium nihil commodi cum expedita magnam officia voluptas ipsam alias aut, veritatis fugit ea facere vitae amet quibusdam! Consequuntur molestias unde similique, illum eos blanditiis perferendis ab, corporis voluptatum optio veritatis a, atque nobis officiis odio suscipit ipsum veniam architecto animi. Minus reiciendis obcaecati quidem quia laudantium quos optio exercitationem.</p>
+                <div className='col-span-2'>
+                    <h2 className='text-2xl font-semibold'>Writer Name Here</h2>
+                    <hr className='mb-3' />
+                    <p className='flex text-left'><span className='w-1/3 md:w-1/4 font-semibold'>Address</span> <span className='w-1/3 md:w-1/4'>Dhaka, Newmarket</span></p>
+                    <p className='flex text-left'><span className='w-1/3 md:w-1/4 font-semibold'>Date of Birth</span> <span className='w-1/3 md:w-1/4'>10 july, 1950</span></p>
+                    <p className='flex text-left'><span className='w-1/3 md:w-1/4 font-semibold'>Date of Dead</span> <span className='w-1/3 md:w-1/4'>09 Auguest, 2000</span></p>
+                    <p className='flex text-left'><span className='w-1/3 md:w-1/4 font-semibold'>Address</span> <span className='w-1/3 md:w-1/4'>Dhaka, Newmarket</span></p>
+                    <p className='text-sm text-gray-600 py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio fuga dicta dolor nisi accusantium velit expedita tempore autem quibusdam dolorem pariatur neque a sunt dolore, excepturi consequuntur. Doloribus modi iusto veritatis impedit, molestiae libero laborum pariatur alias cumque ipsum porro id, voluptas blanditiis. Cum non, nemo, odit dicta fugiat voluptatum a harum commodi distinctio tenetur ut impedit quasi, eos laborum ad est quod provident culpa! Natus, dolorem voluptate. Sint, consectetur adipisci veritatis quia placeat dicta earum eligendi. Porro consequuntur unde iusto ab mollitia, iste dolor, omnis quod delectus laboriosam sed, dicta alias dolorem modi! Impedit iste nemo voluptatibus maiorenam officia voluptas ipsam alias aut, veritatis fugit ea facere vitae amet quibusdam! Consequuntur molestias unde similique, illum eos blanditiis perferendis ab, corporis voluptatum optio veritatis a, atque nobis officiis odio suscipit ipsum veniam architecto animi. Minus reiciendis obcaecati quidem quia laudantium quos optio exercitationem.</p>
                 </div>
             </div>
 
