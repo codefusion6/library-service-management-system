15307a447a094eaa7c2325cce54eeaabd2fe3b98
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-02-08 16:24:54 +0600

    delete search

--- src/app/(root)/allBooks/Search.jsx
+++ src/app/(root)/allBooks/Search.jsx
@@ -1,42 +1,12 @@
 "use client";
-import { getAllBooks } from "@/libs/actions/book.action";
 import { FaSearch } from "react-icons/fa";
-import { useState, useEffect } from "react";
 
 const Search = () => {
   // const books = await getAllBooks();
 
-  const [books, setBooks] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filteredBooks, setFilteredBooks] = useState([]);
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const data = await getAllBooks();
-        setBooks(data.books);
-      } catch (error) {
-        console.error("Error fetching books:", error);
-      }
-    };
 
-    fetchBooks();
-  }, []);
-
-  useEffect(() => {
-    if (!searchTerm) {
-      setFilteredBooks(books);
-    } else {
-      const filtered = books.filter((book) =>
-        book.bookName.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setFilteredBooks(filtered);
-    }
-  }, [searchTerm, books]);
-
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
+  
 
   return (
     <div className="relative rounded-md shadow-sm">
@@ -44,9 +14,9 @@ const Search = () => {
         <FaSearch className="h-5 w-5 text-gray-400" aria-hidden="true" />
       </div>
       <input
-        value={searchTerm}
+      
         placeholder="Search for books"
-        onChange={handleSearch}
+        
         className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
       />
       <div>
