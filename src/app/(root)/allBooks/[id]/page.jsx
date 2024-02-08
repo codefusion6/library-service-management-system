948e46a431a3deed89bdfcb2754774c40b972c54
hossainAbirr <hossainabirbusiness@gmail.com>
2024-02-08 21:12:50 +0600

    build checked

--- src/app/(root)/allBooks/[id]/page.jsx
+++ src/app/(root)/allBooks/[id]/page.jsx
@@ -7,55 +7,6 @@ import { FaRegStar } from "react-icons/fa";
 // import Image from "next/image";
 
 const page = async ({ params }) => {
-<<<<<<< HEAD
-    const singleBook = await getBook(params.id);
-    return (
-        <main className='min-h-screen pt-32'>
-            <div className='container mx-auto'>
-                <div>
-                    <div className="w-full  flex flex-col md:flex-row md:w-2/3 mx-auto bg-white shadow-lg rounded-lg border hover:border-green-500 hover:border-2">
-                        <div className="w-full  md:max-w-[300px] p-2">
-                            <Image
-                                src={singleBook.bookCover}
-                                alt="Card Image"
-                                width={270}
-                                height={270}
-                                style={{ opacity: "1" }}
-                                className="w-full h-full object-cover object-center"
-                            />
-                        </div>
-                        <div className="flex-1 p-2">
-                            <p className="text-lg uppercase font-bold">{singleBook.bookName}</p>
-                            <span>By</span>
-                            <h4 className="font-bold text-large">{singleBook.authorName}</h4>
-                            <p className="text-base font-medium">
-                                Price: <span className="font-bold">{singleBook.category}</span>
-                            </p>
-                            <p className="text-yellow-500 flex">
-                                <FaStar></FaStar>
-                                <FaStar />
-                                <FaStar />
-                                <FaStar />
-                                <FaStar />
-                            </p>
-                            <div className="flex my-5 gap-x-3">
-                                <Button
-                                    variant="ghost"
-                                    className="hover:bg-gradient-to-r border from-yellow-200 to-green-600  hover:text-white text-black font-bold py-2 px-4 rounded"
-                                    size="lg"
-                                >
-                                    Buy
-                                </Button>
-                                <Link href={`/allBooks/${singleBook._id}/view-pdf`} target="_blank" className="hover:bg-gradient-to-r from-yellow-200 to-green-600  hover:text-white text-black font-bold py-2 px-4 rounded border">
-                                    <Button>
-                                        View Pdf
-                                    </Button>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-=======
   const singleBook = await getBook(params.id);
   // console.log(singleBook);
 
@@ -91,7 +42,6 @@ const page = async ({ params }) => {
           <div className="flex-1 space-y-4">
             <div className="text-2xl uppercase font-bold">
               {singleBook.bookName}
->>>>>>> 29b49da3ae6d6d12f9c0e5cde56c5cb4045a134b
             </div>
             <span>By</span>
             <h4 className="font-bold text-large">{singleBook.authorName}</h4>
