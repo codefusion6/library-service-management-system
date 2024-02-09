927e8dc8f28ace51ec5a33c237532845de6677ca
sumonpaul55 <sumpaul100@gmail.com>
2024-02-09 19:02:42 +0600

    import comented

--- src/app/(admin)/dashboard/favourite/page.jsx
+++ src/app/(admin)/dashboard/favourite/page.jsx
@@ -1,4 +1,4 @@
-import FavouriteBooks from '@/components/favouriteBooks/FavouriteBooks'
+// import FavouriteBooks from '@/components/favouriteBooks/FavouriteBooks'
 import React from 'react'
 
 const page = () => {
