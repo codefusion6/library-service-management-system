5f23935223a52110cff80bab2d51626df4835e67
hossainAbirr <hossainabirbusiness@gmail.com>
2024-02-09 21:04:35 +0600

    dashboard sidebar color adjusted

--- src/app/(admin)/dashboard/SidebarItems.js
+++ src/app/(admin)/dashboard/SidebarItems.js
@@ -9,85 +9,100 @@ const SidebarItems = ({ expanded }) => {
   const user = "admin";
   return (
     <>
-      {
-        user === 'user' ? <>
+      {user === "user" ? (
+        <>
           <ul className="text-black space-y-4">
-            <li
-              className="flex gap-2 items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-yellow-100">
+            <li className="flex justify-center gap-2 items-center p-2 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-green-500 hover:text-white bg-[#18c96433] text-[#12a150]">
               <Link href="/" className="flex gap-2 font-bold">
-                <FaHome size={25} className="text-xl font-bold" />
+                <FaHome size={25} className="text-xl font-bold mx-2" />
                 <sapn className={`${expanded ? "block" : "hidden"}`}>Home</sapn>
               </Link>
             </li>
-            <li
-              className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer transition-colors group hover:bg-yellow-100">
-              <Link href="/dashboard/favourite" className="flex gap-2 font-bold">
+            <li className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer transition-colors group hover:bg-green-500 hover:text-white bg-[#18c96433] text-[#12a150]">
+              <Link
+                href="/dashboard/favourite"
+                className="flex gap-2 font-bold"
+              >
                 <CiBoxList size={25} className="text-xl font-bold" />
-                <sapn className={`${expanded ? "block" : "hidden"}`}>Favorite Books</sapn>
+                <sapn className={`${expanded ? "block" : "hidden"}`}>
+                  Favorite Books
+                </sapn>
               </Link>
             </li>
           </ul>
-
-        </> :
-          // admin related route
-          <ul className="text-black space-y-4">
-            <li
-              className="flex gap-2 items-center py-2 px-3 my-1
+        </>
+      ) : (
+        // admin related route
+        <ul className="text-black space-y-4">
+          <li
+            className="flex gap-2 items-center py-2 px-3 my-1
   font-medium rounded-md cursor-pointer
-  transition-colors group hover:bg-yellow-100"
-            >
-              <Link href="/dashboard" className="flex gap-2 font-bold">
-                <FaHome size={25} className="text-xl font-bold" />
-                <sapn className={`${expanded ? "block" : "hidden"}`}>Admin Home</sapn>
-              </Link>
-            </li>
-            <li
-              className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer
-  transition-colors group hover:bg-yellow-100"
-            >
-              <Link href="/dashboard/booklist" className="flex gap-2 font-bold">
-                <CiBoxList size={25} className="text-xl font-bold" />
-                <sapn className={`${expanded ? "block" : "hidden"}`}>Book List</sapn>
-              </Link>
-            </li>
-            <li
-              className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer
-  transition-colors group hover:bg-yellow-100"
-            >
-              <Link href="/dashboard/addbook" className="flex gap-2 font-bold">
-                <MdFormatListBulletedAdd size={25} className="text-xl font-bold" />
-                <sapn className={`${expanded ? "block" : "hidden"}`}>Add Book</sapn>
-              </Link>
-            </li>
-            <li
-              className="flex gap-2 items-center py-2 px-3 my-1
+  transition-colors group hover:bg-green-500 hover:text-white bg-[#18c96433] text-[#12a150]"
+          >
+            <Link href="/dashboard" className="flex gap-2 font-bold">
+              <FaHome size={25} className="text-xl font-bold" />
+              <sapn className={`${expanded ? "block" : "hidden"}`}>
+                Admin Home
+              </sapn>
+            </Link>
+          </li>
+          <li
+            className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer
+  transition-colors group hover:bg-green-500 hover:text-white bg-[#18c96433] text-[#12a150]"
+          >
+            <Link href="/dashboard/booklist" className="flex gap-2 font-bold">
+              <CiBoxList size={25} className="text-xl font-bold" />
+              <sapn className={`${expanded ? "block" : "hidden"}`}>
+                Book List
+              </sapn>
+            </Link>
+          </li>
+          <li
+            className="flex gap-2 font-bold items-center py-2 px-3 my-1 rounded-md cursor-pointer
+  transition-colors group hover:bg-green-500 hover:text-white bg-[#18c96433] text-[#12a150]"
+          >
+            <Link href="/dashboard/addbook" className="flex gap-2 font-bold">
+              <MdFormatListBulletedAdd
+                size={25}
+                className="text-xl font-bold"
+              />
+              <sapn className={`${expanded ? "block" : "hidden"}`}>
+                Add Book
+              </sapn>
+            </Link>
+          </li>
+          <li
+            className="flex gap-2 items-center py-2 px-3 my-1
       font-medium rounded-md cursor-pointer
-      transition-colors group hover:bg-green-100"
-            >
-
-              <Link href="/dashboard/all-user" className="flex gap-2 font-bold"
-              >
-                <CiBoxList size={20} className="text-xl font-bold" />
-                <sapn className={`${expanded ? "block" : "hidden"}`}> All Users</sapn>
-              </Link>
-            </li>
-            <li
-              className="flex gap-2 items-center py-2 px-3 my-1
+      transition-colors group hover:bg-green-500 hover:text-white  bg-[#18c96433] text-[#12a150]"
+          >
+            <Link href="/dashboard/all-user" className="flex gap-2 font-bold">
+              <CiBoxList size={20} className="text-xl font-bold" />
+              <sapn className={`${expanded ? "block" : "hidden"}`}>
+                {" "}
+                All Users
+              </sapn>
+            </Link>
+          </li>
+          <li
+            className="flex gap-2 items-center py-2 px-3 my-1
       font-medium rounded-md cursor-pointer
-      transition-colors group hover:bg-green-100"
+      transition-colors group hover:bg-green-500 hover:text-white bg-[#18c96433] text-[#12a150]"
+          >
+            <Link
+              href="/dashboard/payment-history"
+              className="flex gap-2 font-bold"
             >
-
-              <Link href="/dashboard/payment-history" className="flex gap-2 font-bold"
-              >
-                <CiBoxList size={20} className="text-xl font-bold" />
-                <sapn className={`${expanded ? "block" : "hidden"}`}> Payment History</sapn>
-              </Link>
-            </li>
-
-          </ul>
-      }
+              <CiBoxList size={20} className="text-xl font-bold" />
+              <sapn className={`${expanded ? "block" : "hidden"}`}>
+                {" "}
+                Payment History
+              </sapn>
+            </Link>
+          </li>
+        </ul>
+      )}
     </>
-
   );
 };
 
