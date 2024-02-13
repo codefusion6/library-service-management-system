781a0a286631891e799e4cc8df2889291cf62117
sumonpaul55 <sumpaul100@gmail.com>
2024-02-14 02:25:13 +0600

    payment history not complete yet

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -5,23 +5,24 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 export const POST = async (request) => {
     try {
         const data = await request.json();
-
+        // console.log(data.userName)
         const customer = await stripe.customers.create({
-            email: data?.email,
+            "name": data?.userName,
+            "email": data?.email,
             address: {
                 city: "Mymensingh",
                 country: "Bangladesh",
             },
-            name: "sumon paul",
-            phone: "01628883217"
+
         });
         // create a session for stripe
         const checkOutSession = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             customer: customer.id,
             mode: "payment",
-            success_url: "https://library-service-management-system.vercel.app/successPyment",
-            cancel_url: "https://library-service-management-system.vercel.app/cancel?token" + customer.id,
+            // "https://library-service-management-system.vercel.app"
+            success_url: "http://localhost:3000/successPyment",
+            cancel_url: "http://localhost:3000/cancel?token" + customer.id,
             line_items: [
                 {
                     quantity: 1,
@@ -36,7 +37,7 @@ export const POST = async (request) => {
             ]
         })
         // checkout url
-        return NextResponse.json({ message: checkOutSession, url: checkOutSession.url, status: 200 })
+        return NextResponse.json({ checkOutSession, url: checkOutSession.url, status: 200 })
     } catch (error) {
         return NextResponse.json({ message: error.message, status: 500 })
     }
