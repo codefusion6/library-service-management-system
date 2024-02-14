98586bc057b7b48db798f0b19da7113ff8fc683a
sumonpaul55 <sumpaul100@gmail.com>
2024-02-14 13:57:08 +0600

    data to bookdata

--- src/app/(admin)/dashboard/favourite/FavouriteBook.jsx
+++ src/app/(admin)/dashboard/favourite/FavouriteBook.jsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 import React from 'react'
 import Swal from 'sweetalert2'
 
-const FavouriteBook = ({ data }) => {
-    const book = data;
+const FavouriteBook = ({ bookData }) => {
+
     // console.log(book)
     // const { user } = UserAuth()
     // console.log(book)
@@ -28,13 +28,13 @@ const FavouriteBook = ({ data }) => {
     // }
     return (
         <div className='flex gap-4 p-2 border my-3'>
-            <Image className='max-w-[200px]' src={book?.bookCover} width={300} height={600} alt={book.bookName} />
+            <Image className='max-w-[200px]' src={bookData?.bookCover} width={300} height={600} alt={bookData.bookName} />
             <div className='flex-1 p-4 flex flex-col justify-between'>
                 <div className='space-y-3'>
-                    <h2 className='sm:text-xl md:text-2xl font-semibold'>{book?.bookName}</h2>
-                    <h3 className='font-medium'>Author: {book?.authorName}</h3>
-                    <p className='text-sm'>{book.bookDescription}</p>
-                    <h3 className='text-primary font-bold'>Category: {book?.category}</h3>
+                    <h2 className='sm:text-xl md:text-2xl font-semibold'>{bookData?.bookName}</h2>
+                    <h3 className='font-medium'>Author: {bookData?.authorName}</h3>
+                    <p className='text-sm'>{bookData.bookDescription}</p>
+                    <h3 className='text-primary font-bold'>Category: {bookData?.category}</h3>
                 </div>
                 <div className='flex gap-5 mt-10'>
                     <Link className='py-1 px-3 md:px-5 bg-primary text-white roumded-md cursor-pointer'>View Details</Link>
