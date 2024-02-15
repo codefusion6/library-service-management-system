78cbfb4976e5d396bbbba879dd08fc524a94ea01
sumonpaul55 <sumpaul100@gmail.com>
2024-02-16 03:53:30 +0600

    tabel edited

--- src/app/(admin)/dashboard/payment-history/page.jsx
+++ src/app/(admin)/dashboard/payment-history/page.jsx
@@ -23,12 +23,12 @@ const PaymentHistory = async () => {
               data?.map((items, idx) => (
                 <tr key={idx} className='even:bg-slate-300'>
                   <td className='py-2 px-4 border-b'>
-                    <Image src={items?.userProfile} alt={`user`} width={30} height={30} className='size-16 rounded-full' />
+                    <Image src={items?.userProfile} alt={`user`} width={30} height={30} className='size-16 rounded-full mx-auto' />
                   </td>
-                  <td className="py-2 px-4 border-b">{items.paymentId}</td>
-                  <td className="py-2 px-4 border-b">{items?.createdAt}</td>
-                  <td className="py-2 px-4 border-b">{items?.userName}</td>
-                  <td className="py-2 px-4 border-b">{items?.email}</td>
+                  <td className="py-2 text-center px-4 border-b">{items.paymentId}</td>
+                  <td className="py-2 text-center px-4 border-b">{items?.createdAt}</td>
+                  <td className="py-2 text-center px-4 border-b">{items?.userName}</td>
+                  <td className="py-2 text-center px-4 border-b">{items?.email}</td>
                 </tr>
               ))
             }
