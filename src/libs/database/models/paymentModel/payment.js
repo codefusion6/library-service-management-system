c6cd954aa359736b5204b73a94d4a513ff497d87
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-02-16 14:05:34 +0600

    write the backend api for update user

--- src/libs/database/models/paymentModel/payment.js
+++ src/libs/database/models/paymentModel/payment.js
@@ -1,12 +1,12 @@
 import { model, models, Schema } from "mongoose";
 
 const PaymentSchema = new Schema({
-    paymentId: { type: String, required: true},
+    userName: { type: String, required: true},
     paymentDate: { type: String, required: true},
     userProfile : { type: String, required: true},
     userName: { type: String, required: true},
     emailAddress: {type: String, required: true},
-    paymentAmount : { type: String, required: true } 
+    paymentAmount : { type: String, required: true }, 
 
 })
 
