357eda06b45af7dfda84db1a89e6566d4f5d83e1
sumonpaul55 <sumpaul100@gmail.com>
2024-02-27 23:29:41 +0600

    memebership problem fixed in local

--- src/libs/actions/user.actions.js
+++ src/libs/actions/user.actions.js
@@ -149,20 +149,16 @@ export const updateUserProfile = async (formData, useremail) => {
 };
 
 export const getUserAndBecomeMember = async (email) => {
+  console.log(email)
   try {
     await connectDB();
 
-    const result = await User.findOneAndUpdate({ email: email }, { role: "member" }, { new: true }, (err, user) => {
-      if (err) {
-        return JSON.parse(JSON.stringify(err))
-      } else {
-        return JSON.parse(JSON.stringify({ success: true }))
-      }
-    });
-
+    const result = await User.findOneAndUpdate({ email: email }, { role: "member" }, { new: true });
+    console.log(JSON.parse(JSON.stringify(result)))
+    return JSON.parse(JSON.stringify({ success: true }))
 
   } catch (error) {
-    return JSON.parse(JSON.stringify(error))
+    return { error: "An error occurred while updating user role." };
   }
 }
 
