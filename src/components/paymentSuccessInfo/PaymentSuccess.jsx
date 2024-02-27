19bcf9d21a498376f3e8db56c7272c8b7686ddff
sumonpaul55 <sumpaul100@gmail.com>
2024-02-27 21:05:48 +0600

    link removed

--- src/components/paymentSuccessInfo/PaymentSuccess.jsx
+++ src/components/paymentSuccessInfo/PaymentSuccess.jsx
@@ -4,7 +4,7 @@ import { UserAuth } from '@/app/provider/context/AuthContext'
 import { addPaymentHistory, getOnePaymentHistory } from '@/libs/actions/payment.action'
 // import { getUserMemberShip } from '@/libs/actions/user.actions'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { redirect, usePathname } from 'next/navigation'
 import React, { } from 'react'
 // import { useEffect } from 'react'
 import toast from 'react-hot-toast'
@@ -50,6 +50,7 @@ const PaymentSuccess = ({ payments, alredyExitData }) => {
                         toast.success('Your are Now member of BookFlow', {
                             position: "bottom-right"
                         })
+                        redirect("/")
                     }
                 }
             }
@@ -92,11 +93,9 @@ const PaymentSuccess = ({ payments, alredyExitData }) => {
                     <Confetti height="1000" width="1500" className='w-full' opacity={.4} />
                     <p className='my-5 pt-10 text-primary md:text-2xl lg:text-3xl'>You are now Member of BookFlw. <span className='text-pink-500 font-bold'>{paymentss.amount_received === 3000 ? 7 : paymentss.amount_received === 1000 ? 30 : paymentss.amount_received === 5000 ? 60 : null} Days</span></p>
                     <div className='flex gap-8 justify-center pt-10'>
-                        <Link href="/allBooks">
-                            <button onClick={() => handlepaymentHistory(paymentHistory)} className='py-2 px-4 md:px-7 text-white bg-pink-600 text-xl hover:bg-lime-600 duration-200 hover:text-white rounded-lg'>
-                                confirm
-                            </button>
-                        </Link>
+                        <button onClick={() => handlepaymentHistory(paymentHistory)} className='py-2 px-4 md:px-7 text-white bg-pink-600 text-xl hover:bg-lime-600 duration-200 hover:text-white rounded-lg'>
+                            confirm
+                        </button>
                         {/* {
                             <div className='flex gap-3'>
                                 <Link href="/" className='py-2 px-4 md:px-7 text-white bg-pink-600 text-xl hover:bg-lime-600 duration-200 hover:text-white rounded-lg'>
