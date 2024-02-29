cde58fa3f4c27b168bff21dcbad4416fe60c573e
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-01 00:13:45 +0600

    working on backend

--- src/libs/actions/acceptPublisherRequest.action.js
+++ src/libs/actions/acceptPublisherRequest.action.js
@@ -1,32 +1,86 @@
 "use server";
 import { connectDB } from "../database/MongoConnect";
 import PublisherRequest from "../database/models/publisherRequestModel/publisherRequestSchema";
+import BookRequest from "../database/models/requestBookModel/requestBook";
 import { updateUserRole } from "./user.actions";
-
 
 export const acceptPublisherRequest = async (requestId) => {
   try {
     await connectDB();
-
     const requestToUpdate = await PublisherRequest.findById(requestId);
-
     if (!requestToUpdate) {
-      return JSON.parse(JSON.stringify({ success: false, error: 'Request not found' }));
+      return JSON.parse(
+        JSON.stringify({ success: false, error: "Request not found" })
+      );
     }
 
-    requestToUpdate.status = 'accepted';
+    requestToUpdate.status = "accepted";
 
     const updatedRequest = await requestToUpdate.save();
 
     // Add the publisher role to the user associated with the request
-    const result = await updateUserRole(requestToUpdate.email, 'publisher');
+    const result = await updateUserRole(requestToUpdate.email, "publisher");
 
     if (result.success) {
-      return JSON.parse(JSON.stringify({ success: true, data: updatedRequest }));
+      return JSON.parse(
+        JSON.stringify({ success: true, data: updatedRequest })
+      );
     } else {
-      return JSON.parse(JSON.stringify({ success: false, error: 'Failed to update user role', errorDetails: result.error }));
+      return JSON.parse(
+        JSON.stringify({
+          success: false,
+          error: "Failed to update user role",
+          errorDetails: result.error,
+        })
+      );
     }
   } catch (error) {
-    return JSON.parse(JSON.stringify({ success: false, error: 'Error accepting request', errorDetails: error }));
+    return JSON.parse(
+      JSON.stringify({
+        success: false,
+        error: "Error accepting request",
+        errorDetails: error,
+      })
+    );
+  }
+};
+
+
+// book request changing status
+export const acceptBookRequest = async (requestId) => {
+  try {
+    await connectDB();
+
+    const requestToUpdate = await BookRequest.findById(requestId);
+
+    if (!requestToUpdate) {
+      return JSON.parse(
+        JSON.stringify({ success: false, error: "Request not found" })
+      );
+    }
+
+    requestToUpdate.status = "accepted";
+
+    if (result.success) {
+      return JSON.parse(
+        JSON.stringify({ success: true, data: updatedRequest })
+      );
+    } else {
+      return JSON.parse(
+        JSON.stringify({
+          success: false,
+          error: "Failed to update user role",
+          errorDetails: result.error,
+        })
+      );
+    }
+  } catch (error) {
+    return JSON.parse(
+      JSON.stringify({
+        success: false,
+        error: "Error accepting request",
+        errorDetails: error,
+      })
+    );
   }
 };
--- src/libs/actions/rejectPublisherRequest.action.js
+++ src/libs/actions/rejectPublisherRequest.action.js
@@ -1,33 +1,70 @@
-"use server"
+"use server";
 import { connectDB } from "../database/MongoConnect";
 import PublisherRequest from "../database/models/publisherRequestModel/publisherRequestSchema";
+import BookRequest from "../database/models/requestBookModel/requestBook";
 import { updateUserRole } from "./user.actions";
 
 export const rejectPublisherRequest = async (requestId) => {
-    try {
-      await connectDB();
-  
-      const requestToUpdate = await PublisherRequest.findById(requestId);
-  
-      if (!requestToUpdate) {
-        return JSON.parse(JSON.stringify({ success: false, error: 'Request not found' }));
-      }
-  
-      // Revert the user's role to the default role (e.g., 'user')
-      const result = await updateUserRole(requestToUpdate.email, 'user');
-  
-      if (!result.success) {
-        return JSON.parse(JSON.stringify({ success: false, error: 'Failed to update user role', errorDetails: result.error }));
-      }
-  
-      // Set the status to 'pending' before saving
-      requestToUpdate.status = 'pending';
-  
-      // Save the updated request
-      const updatedRequest = await requestToUpdate.save();
-  
-      return JSON.parse(JSON.stringify({ success: true, data: updatedRequest }));
-    } catch (error) {
-      return JSON.parse(JSON.stringify({ success: false, error: 'Error rejecting request', errorDetails: error }));
+  try {
+    await connectDB();
+    const requestToUpdate = await PublisherRequest.findById(requestId);
+
+    if (!requestToUpdate) {
+      return JSON.parse(
+        JSON.stringify({ success: false, error: "Request not found" })
+      );
     }
-  };
+
+    // Revert the user's role to the default role (e.g., 'user')
+    const result = await updateUserRole(requestToUpdate.email, "user");
+
+    if (!result.success) {
+      return JSON.parse(
+        JSON.stringify({
+          success: false,
+          error: "Failed to update user role",
+          errorDetails: result.error,
+        })
+      );
+    }
+
+    // Set the status to 'pending' before saving
+    requestToUpdate.status = "pending";
+
+    // Save the updated request
+    const updatedRequest = await requestToUpdate.save();
+
+    return JSON.parse(JSON.stringify({ success: true, data: updatedRequest }));
+  } catch (error) {
+    return JSON.parse(
+      JSON.stringify({
+        success: false,
+        error: "Error rejecting request",
+        errorDetails: error,
+      })
+    );
+  }
+};
+
+export const rejectBookRequest = async (requestId) => {
+  try {
+    await connectDB();
+    const requestToUpdate = await BookRequest.findById(requestId);
+
+    if (!requestToUpdate) {
+      return JSON.parse(
+        JSON.stringify({ success: false, error: "Request not found" })
+      );
+    }
+
+    return JSON.parse(JSON.stringify({ success: true, data: updatedRequest }));
+  } catch (error) {
+    return JSON.parse(
+      JSON.stringify({
+        success: false,
+        error: "Error rejecting request",
+        errorDetails: error,
+      })
+    );
+  }
+};
