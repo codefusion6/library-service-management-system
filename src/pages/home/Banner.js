10dce25314adc9230d9090ed8b0f39c2bd8e56ec
rafahuqe <arinahuqe23@gmail.com>
2024-03-01 15:23:59 +0600

    pull from development

--- src/pages/home/Banner.js
+++ src/pages/home/Banner.js
@@ -1,6 +1,7 @@
 "use client"
 import BannerSlider from "@/components/Home/Slider/BannerSlider";
 import { Button } from "@nextui-org/react";
+import Link from "next/link";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
@@ -28,21 +29,21 @@ const Banner = () => {
                 awaits!&quot;
               </p>
               <div className="flex gap-5 font-medium">
-                <Button
-                  color="primary"
-                  variant="bordered"
-                  className="font-bold py-2 px-4 rounded-md border-1 border-[#1ba752] md:px-6"
-                  size="lg"
-                >
-                  Explore
-                </Button>
-                <Button
-                  color="primary"
-                  className="bg-gradient-to-tr from-yellow-200 to-[#1ba752] text-white shadow-lg py-2 px-4 md:px-6 rounded-md"
-                  size="lg"
-                >
-                  Buy Now
-                </Button>
+                <Link href="/allBooks">
+               
+                <button className="text-xl w-32 h-16 bg-black text-white relative overflow-hidden group z-10 rounded-lg">
+                  <span className="absolute bg-white rotate-12
+                 -inset-8 group-hover:duration-300 duration-700 scale-x-0 group-hover:scale-x-100 
+                 origin-left transform transition-transform"></span><span className="absolute bg-green-700 
+                 rotate-12 -inset-8 group-hover:duration-700 duration-500 scale-x-0 group-hover:scale-x-100
+                  origin-left transform transition-transform"></span><span className="absolute bg-green-900 
+                  rotate-12 -inset-8 group-hover:duration-500 duration-300 scale-x-0 group-hover:scale-x-50
+                   origin-left transform transition-transform"></span><span className="absolute opacity-0 
+                   group-hover:opacity-100 duration-100 group-hover:duration-700 ease-out text-center z-10
+                    text-white">All Books</span> Explore</button>
+                </Link>
+
+                
               </div>
             </div>
           </div>
