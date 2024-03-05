f907aaeba3ebca528b4bd3754215fc0a09b4d42c
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-05 20:59:56 +0600

    contact footer fixed

--- src/pages/home/Banner.js
+++ src/pages/home/Banner.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import BannerSlider from "@/components/Home/Slider/BannerSlider";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
@@ -12,13 +12,7 @@ const Banner = () => {
         <div className="justify-content-center align-items-center grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-3">
           <div className="flex flex-col justify-around gap-2 px-5 lg:px-0">
             <h2 className="text-3xl md:min-h-20 sm:text-7xl lg:text-5xl xl:text-7xl py-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-200 to-green-600">
-              <TypeAnimation
-                sequence={["", 1000, "Library Service Management", 1000]}
-                wrapper="span"
-                speed={50}
-                style={{ display: "inline-block" }}
-                repeat={Infinity}
-              />
+              Library Service Management
             </h2>
             <div className="flex flex-col gap-5 lg:gap-12">
               <p className="md:text-xl font-base md:font-normal text-left">
@@ -29,26 +23,16 @@ const Banner = () => {
                 awaits!&quot;
               </p>
               <div className="flex gap-5 font-medium">
-              <Link href="/allBooks">
-               
-               <button className="text-xl w-32 h-12 bg-[#1E293B] text-white relative overflow-hidden group z-10 rounded-lg">
-                 <span className="absolute bg-white rotate-12
-                -inset-8 group-hover:duration-300 duration-700 scale-x-0 group-hover:scale-x-100 
-                origin-left transform transition-transform"></span><span className="absolute bg-green-700 
-                rotate-12 -inset-8 group-hover:duration-700 duration-500 scale-x-0 group-hover:scale-x-100
-                 origin-left transform transition-transform"></span><span className="absolute bg-green-900 
-                 rotate-12 -inset-8 group-hover:duration-500 duration-300 scale-x-0 group-hover:scale-x-50
-                  origin-left transform transition-transform"></span><span className="absolute opacity-0 
-                  group-hover:opacity-100 duration-100 group-hover:duration-700 ease-out text-center z-10
-                   text-white">All Books</span> Explore</button>
-               </Link>
-                {/* <Button
-                  color="primary"
-                  className="bg-gradient-to-tr from-yellow-200 to-[#1ba752] text-white shadow-lg py-2 px-4 md:px-6 rounded-md"
-                  size="lg"
-                >
-                  Buy Now
-                </Button> */}
+                <Link href="/allBooks">
+                  <button className="text-xl w-28 h-12 before:block before:absolute bg-[#f3f0f0] hover:text-sky-900 before:w-0 before:h-0 hover:before:h-20 hover:before:w-40 before:-bottom-2 before:-right-2 before:duration-500 before:rounded-xl before:-z-10 relative inline-block transform text-sky-500 bg-transparent border-2 overflow-hidden border-sky-700 duration-500">
+                    Button
+                  </button>
+                </Link>
+                <Link href="">
+                  <button className="text-xl w-28 h-12 bg-sky-700 hover:scale-95 text-white duration-300">
+                    Button
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
