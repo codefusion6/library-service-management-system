78d7d61b1c614d237b235c789a840be6f01c58d6
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-05 20:28:00 +0600

    contact us page banner fixed

--- src/pages/home/recomented/RecommendedBooks.jsx
+++ src/pages/home/recomented/RecommendedBooks.jsx
@@ -7,9 +7,9 @@ const RecommendedBooks = () => {
   return (
     <section className="min-h-screen">
       <div className="container mx-auto">
-        <p className="text-2xl font-bold pb-2 pl-5 lg:pl-0">Recommenced Book&apos;s </p>
-        <hr />
-        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-5 lg:p-0">
+        <p className="text-3xl font-bold pb-2 pl-5 lg:pl-0">Recommenced Book&apos;s </p>
+      
+        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 p-5 lg:p-0">
           <Cards></Cards>
         </div>
       </div>
