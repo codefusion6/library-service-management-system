12a3091d4c67fc169e17d5bb03c125a0f52c85f4
rafahuqe <arinahuqe23@gmail.com>
2024-03-07 14:31:38 +0600

    Revamped user profile page: A fresh look with added edit functionality. Let users personalize their online presence! 🚀

--- src/app/(root)/contact/page.jsx
+++ src/app/(root)/contact/page.jsx
@@ -11,32 +11,23 @@ import { TypeAnimation } from "react-type-animation";
 const page = () => {
   return (
     <>
-      <ContactBanner></ContactBanner>
       <section>
-        <div></div>
-      </section>
-
-      
-
-      <div className="">
-        <div className="">
-          <div className="">
-            <div>
-              
-              <div className="container flex flex-col mx-auto bg-white">
-                <div className=" ">
-                  <Talk />
-                </div>
-              </div>
-            </div>
-            <div className=" mt-4 items-center justify-center place-items-center">
-              <div className=" gap-2 justify-center md:w-3/4 mx-auto">
-                <Feedback />
-              </div>
+      <ContactBanner></ContactBanner>
+      <div>
+        <div>
+          <div className="container flex flex-col mx-auto bg-white">
+            <div className=" ">
+              <Talk />
             </div>
           </div>
         </div>
+        <div className=" mt-4 items-center justify-center place-items-center">
+          <div className=" gap-2 justify-center md:w-3/4 mx-auto">
+            <Feedback />
+          </div>
+        </div>
       </div>
+      </section>
     </>
   );
 };
